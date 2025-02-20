07671353b3dcc9ebeee6ff2bd43fecfb6d902f45
Plamen <plamy5bg@gmail.com>
2025-02-20 09:32:16 +0200

    asteroid logic script

---
+++ Assets/Scripts/Asteroid spawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float spawnRate = 1.0f; 
+    public float spawnRangeX = 10f; 
+    public float spawnHeight = 6f; 
+    public int maxAsteroids = 10;
+    public float minSpeed = 2f, maxSpeed = 5f;
+
+    private int _asteroidCount = 0;
+
+    void Start()
+    {
+        StartCoroutine(SpawnAsteroids());
+    }
+
+    IEnumerator SpawnAsteroids()
+    {
+        while (true)
+        {
+            if (_asteroidCount < maxAsteroids)
+            {
+                SpawnAsteroid();
+                _asteroidCount++;
+            }
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+
+    void SpawnAsteroid()
+    {
+        
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        Vector2 spawnPosition = new Vector2(randomX, spawnHeight);
+
+        
+        GameObject asteroid = Instantiate(asteroidPrefab, spawnPosition, Quaternion.identity);
+
+        
+        Rigidbody2D rb = asteroid.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = new Vector2(0, -Random.Range(minSpeed, maxSpeed));
+        }
+    }
+}
