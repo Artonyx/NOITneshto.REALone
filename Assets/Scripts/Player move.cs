008e4d93a8cde70bfbc70351f76d4576b6fb4638
Plamen <plamy5bg@gmail.com>
2025-02-23 16:22:59 +0200

    inefficient my ass

--- Assets/Scripts/Player move.cs
+++ Assets/Scripts/Player move.cs
@@ -5,12 +5,10 @@ public class SpaceshipController : MonoBehaviour
     public float thrustPower = 70f;
     public float rotationSpeed = 100f;
 
-    private Rigidbody2D rb;
     private Rigidbody2D _rb;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         _rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,19 +21,19 @@ public class SpaceshipController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(Vector2.up * thrustPower * Time.deltaTime);
+            _rb.AddForce(Vector2.up * thrustPower * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(Vector2.down * thrustPower * Time.deltaTime);
+            _rb.AddForce(Vector2.down * thrustPower * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(Vector2.right * thrustPower * Time.deltaTime);
+            _rb.AddForce(Vector2.right * thrustPower * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(Vector2.left * thrustPower * Time.deltaTime);
+            _rb.AddForce(Vector2.left * thrustPower * Time.deltaTime);
         }
     }
 
