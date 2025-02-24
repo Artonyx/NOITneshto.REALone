9f2116dd63d8b12e897c7aa15ee631858c9f64bd
Artonyx <135943828+Artonyx@users.noreply.github.com>
2025-02-24 19:09:26 +0200

    push push push

---
+++ Assets/Scripts/checkBox.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkBox : MonoBehaviour
+{
+    public List<questionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    private void Start()
+    {
+        
+    }
+    
+    
+
+}
---
+++ Assets/Scripts/questionsAndAnswers.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+[System.Serializable]
+public class questionsAndAnswers : MonoBehaviour
+{
+    public string question;
+    public string[] Answers;
+    public int correctAnswer;
+}
