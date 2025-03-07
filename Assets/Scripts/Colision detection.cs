f5b3ba8ec49830bc8b9aa8649695b67721395e12
Plamen <plamy5bg@gmail.com>
2025-03-07 11:09:23 +0200

    ???

--- Assets/Scripts/Colision detection.cs
+++ Assets/Scripts/Colision detection.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 
 public class Hitbox2D : MonoBehaviour
 {
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected with: " + collision.collider.name);
+
+        Health health = collision.collider.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(1);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Hit detected with: " + other.name);
-        
+        Debug.Log("Trigger detected with: " + other.name);
+
         Health health = other.GetComponent<Health>();
         if (health != null)
         {
@@ -13,4 +24,3 @@ public class Hitbox2D : MonoBehaviour
         }
     }
 }
-
