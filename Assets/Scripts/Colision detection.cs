1975733a72a25e81e4d0959673eb95fd5f5bc543
Plamen <plamy5bg@gmail.com>
2025-05-05 12:54:25 +0300

    not working :(

--- Assets/Scripts/Colision detection.cs
+++ Assets/Scripts/Colision detection.cs
@@ -1,29 +1,24 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
-public class Hitbox2D : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Collision detected with: " + collision.collider.name);
+public class Hitbox2D : MonoBehaviour { private void OnCollisionEnter2D(Collision2D collision) { Debug.Log($"Collision detected between {gameObject.name} and {collision.collider.name}");
 
-        // Check if the object we collided with is the player
         PlayerHealth playerHealth = collision.collider.GetComponent<PlayerHealth>();
         if (playerHealth != null)
         {
             playerHealth.TakeDamage(1);
-            Destroy(gameObject); // Destroy asteroid on impact
+            Destroy(gameObject);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger detected with: " + other.name);
+        Debug.Log($"Trigger detected between {gameObject.name} and {other.name}");
 
-        PlayerHealth health = other.GetComponent<PlayerHealth>();
-        if (health != null)
+        if (other.CompareTag("Projectile") || other.CompareTag("Rocket"))
         {
-            health.TakeDamage(1);
-            Destroy(gameObject); // Destroy asteroid when hit by a bullet
+            Destroy(gameObject);
         }
     }
+
 }
